1bbbac0a12574a3a099e102b7159cd688ddde046
Martin Terziev <m.terziev08@abv.bg>
2025-09-03 20:12:17 +0300

    Add project files.

---
+++ ExcelReader/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExcelReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

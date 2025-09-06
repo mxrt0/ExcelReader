8236cf1977a6cda74e1861866682fe9c50bd2898
Martin Terziev <m.terziev08@abv.bg>
2025-09-06 19:56:13 +0300

    Polishing/finished.

--- ExcelReader/Program.cs
+++ ExcelReader/Program.cs
@@ -8,8 +8,8 @@ public class Program
     static async Task Main()
     {
         ExcelPackage.License.SetNonCommercialPersonal("mxrt0");
-        var filePath = AnsiConsole.Prompt(new TextPrompt<string>("[aqua]Please insert the file path you wish to seed into/read from: [/]"));
-        var reader = new ProductReader(filePath.Trim('"') ?? @"..\..\..\data.xslx");
+        var filePath = AnsiConsole.Prompt(new TextPrompt<string>("[aqua]Please insert the file path you wish to read from: [/]"));
+        var reader = new ProductReader(filePath.Trim('"') ?? @"..\..\..\products.xslx");
         await reader.Run();
         Console.ReadKey();
     }
--- ExcelReader/Services/ProductReader.cs
+++ ExcelReader/Services/ProductReader.cs
@@ -13,60 +13,9 @@ public class ProductReader
 
     public async Task Run()
     {
-        if (!File.Exists(excelFilePath))
-        {
-            try
-            {
-                await SeedExcelData();
-            }
-            catch (Exception ex)
-            {
-                AnsiConsole.MarkupLine($"[red]Error while locating file path: [bold]{ex.Message}[/][/]");
-                return;
-            }
-        }
         var data = ReadDataFromSpreadsheet(excelFilePath);
         await SaveDataToDatabase(data);
-
         DisplayData(_genericContext!.Items);
-    }
-
-    public async Task SeedExcelData()
-    {
-        int productsCount = 300_000;
-        using var package = new ExcelPackage();
-        var worksheet = package.Workbook.Worksheets.Add("Products");
-
-        worksheet.Cells[1, 1].Value = "ProductName";
-        worksheet.Cells[1, 2].Value = "UnitPrice";
-        worksheet.Cells[1, 3].Value = "Quantity";
-        worksheet.Cells[1, 4].Value = "TotalPrice";
-        worksheet.Cells[1, 5].Value = "TotalPriceWithVAT";
-        worksheet.Cells[1, 6].Value = "RandomHeader";
-
-        int row = 2;
-        AnsiConsole.Progress()
-            .Start(ctx =>
-            {
-                var task = ctx.AddTask("[aqua]Generating [lime][bold]Excel[/][/] Worksheet[/]\n", maxValue: productsCount);
-                //foreach (var product in GenerateProducts(productsCount))
-                //{
-                //    worksheet.Cells[row, 1].Value = product.ProductName;
-                //    worksheet.Cells[row, 2].Value = product.UnitPrice;
-                //    worksheet.Cells[row, 3].Value = product.Quantity;
-                //    worksheet.Cells[row, 4].Value = product.TotalPrice;
-                //    worksheet.Cells[row, 5].Value = product.TotalPriceWithVAT;
-                //    worksheet.Cells[row, 6].Value = product.Random;
-                //    row++;
-                //    task.Increment(1);
-                //}
-            });
-
-        worksheet.Cells[worksheet.Dimension.Address].AutoFitColumns();
-
-        await package.SaveAsAsync(new FileInfo(excelFilePath));
-
-        AnsiConsole.MarkupLine("[aqua]Generated [lime][bold]Excel[/][/] Worksheet![/]\n");
     }
 
     public IEnumerable<Models.ExcelRow> ReadDataFromSpreadsheet(string filePath)
@@ -90,7 +39,7 @@ public class ProductReader
         _genericContext = new GenericDbContext(headers);
         _genericContext.Database.EnsureDeleted();
         _genericContext.Database.EnsureCreated();
-        AnsiConsole.MarkupLine("[aqua]Created [lime][bold]Database Table[/][/]![/]\n");
+        AnsiConsole.MarkupLine("[aqua]Created [lime][bold]Database Table[/][/]![/]");
 
         var rows = new List<Models.ExcelRow>();
         AnsiConsole.Progress()
@@ -133,7 +82,7 @@ public class ProductReader
         var headers = _genericContext!.GetHeaders();
         foreach (string header in headers)
         {
-            table.AddColumn(header);
+            table.AddColumn(header).Centered();
         }
         var entities = items.Select(e => _genericContext!.Entry(e)).Where(e =>
         {
@@ -154,7 +103,7 @@ public class ProductReader
             {
                 rowData[index++] = entity.Property(header).CurrentValue!.ToString()!;
             }
-            table.AddRow(rowData);
+            table.AddRow(rowData).Centered();
         }
         AnsiConsole.MarkupLine($"[aqua]Data:[/]\n");
         AnsiConsole.Write(table);
